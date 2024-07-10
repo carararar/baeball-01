77b506016250cf48262294a1f66aa06ff8921516
car <znargon@gmail.com>
2024-07-10 14:14:00 +0900

    [feature] 41

--- game.py
+++ game.py
@@ -1,6 +1,16 @@
+import re
+
+
 class Game:
     def guess(self, guessNumber):
         if guessNumber is None:
             raise TypeError
         if len(guessNumber) != 3:
+            raise TypeError
+        if re.search(r"\D", guessNumber):
+            raise TypeError
+
+        if guessNumber[0] == guessNumber[1] or \
+                guessNumber[0] == guessNumber[2] or \
+                guessNumber[1] == guessNumber[2]:
             raise TypeError
