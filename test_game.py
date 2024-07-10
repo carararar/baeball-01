bcfe368fa3fd0214d643e379b7101fdf62b2f46b
car <znargon@gmail.com>
2024-07-10 14:45:18 +0900

    [feature] 60

--- test_game.py
+++ test_game.py
@@ -16,6 +16,11 @@ class TestBaseball(TestCase):
         except TypeError:
             pass
 
+    def assert_matched_number(self, result, solved, strikes, balls):
+        self.assertIsNotNone(result)
+        self.assertEqual(solved, result.get_solved())
+        self.assertEqual(strikes, result.get_strikes())
+        self.assertEqual(balls, result.get_balls())
 
     def test_game_should_raise_exception_when_invalid_input(self):
         self.assert_illegal_argument(None)
@@ -25,17 +30,16 @@ class TestBaseball(TestCase):
         self.assert_illegal_argument("121")
 
     def test_game_should_return_solve_result_when_matched_input(self):
-        self.game.question = "123"
-        result: GameResult = self.game.guess("123")
-        self.assertIsNotNone(result)
-        self.assertTrue(result.get_solved())
-        self.assertEqual(3, result.get_strikes())
-        self.assertEqual(0, result.get_balls())
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("123"), True, 3, 0)
 
     def test_game_should_return_solve_result_when_unmatched_input(self):
-        self.game.question = "123"
-        result: GameResult = self.game.guess("456")
-        self.assertIsNotNone(result)
-        self.assertFalse(result.get_solved())
-        self.assertEqual(0, result.get_strikes())
-        self.assertEqual(0, result.get_balls())
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("456"), False, 0, 0)
+
+    def generate_question(self, question_number):
+        self.game.question = question_number
+
+    def test_game_should_return_solve_result_when_some_matched_input(self):
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("120"), False, 2, 0)
