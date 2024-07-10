470f6417cc72376b2aafedb3c89eadf204bf1882
car <znargon@gmail.com>
2024-07-10 13:59:08 +0900

    [feature] 26

--- game.py
+++ game.py
@@ -1,2 +1,4 @@
-class Baseball:
-    pass
+class Game:
+    def guess(self, param):
+        if param is None:
+            raise TypeError
--- test_game.py
+++ test_game.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
+from game import Game
+
 
 class TestBaseball(TestCase):
-    def test_game(self):
-        self.assertEqual(1, 1)
+    def test_baseball_when_input_is_none(self):
+        self.game = Game()
+        with self.assertRaises(TypeError):
+            self.game.guess(None)
+
